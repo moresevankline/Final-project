767cc1908f3363aa4c5245e2f10a0cb4a4b0f3e7
Jhon Paul Joe C. Novida <114741225+Raveneye21@users.noreply.github.com>
2022-12-05 15:17:54 +0800

    Add files via upload

--- Hospital.py
+++ Hospital.py
@@ -1,1 +1,33 @@
+import Read_Hospital_Excel_Sheet
 
+Doctors_DataBase = Read_Hospital_Excel_Sheet.Read_Doctors_DataBase()
+Patients_DataBase = Read_Hospital_Excel_Sheet.Read_Patients_DataBase()
+
+class Hospital:
+
+    def hospital_department():
+        for i in Doctors_DataBase:
+            print(" " + Doctors_DataBase[i][0][0])
+
+    def hospital_doctors():
+        for i in Doctors_DataBase:
+            print (" " + Doctors_DataBase[i][0][1] + " in " + Doctors_DataBase[i][0][0] + 
+                    " department, from " + Doctors_DataBase[i][0][2])
+    
+    def hospital_residents():
+        for i in Patients_DataBase:
+            print("Patient : " + Patients_DataBase[i][2] + " in " + Patients_DataBase[i][0]
+                + " department and followed by " + Patients_DataBase[i][1] + ", age : "
+                + Patients_DataBase[i][3] + ", from " + Patients_DataBase[i][5] + ", Room Number : "
+                + Patients_DataBase[i][6])
+
+    def hospital_menu():
+        print("")
+        print("                -------------------------------------------")
+        print("                | 1. To view hospital's departments       |")
+        print("                | 2. To view hospital's doctors           |")
+        print("                | 3. To view patients' residents          |")
+        print("                | 4. To view patient's details            |")
+        print("                | To Exit Enter B                         |")
+        print("                -------------------------------------------")
+        print("")
