1e3b5e7830e0331fd9ea6ebe704a3c0ca7f829ab
Evan Kline Mores <113956467+moresevankline@users.noreply.github.com>
2022-12-05 15:19:18 +0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -12,89 +12,88 @@ tries = 0
 tries_flag = ""
 
 while tries_flag != "Close the program":
-    print("-----------------------------------------")
-    print("|Enter 1 for Admin mode                 |")
-    print("|Enter 2 for User mode                  |")
-    print("-----------------------------------------")
-    Admin_user_mode = input("Enter your mode : ")
+    print("                 -----------------------------------------")
+    print("                 |         Enter 1 for Admin mode        |")
+    print("                 |         Enter 2 for User mode         |")
+    print("                 |         Enter 3 to Exit               |")
+    print("                 -----------------------------------------")
+    print("")
+    Admin_mode = input("Enter your mode : ")
 
-    if Admin_user_mode == "1":  
-        print("*******************************************")
-        print("|                                         |")
-        print("|         Welcome to admin mode           |")
-        print("|                                         |")
-        print("*******************************************")
+    if Admin_mode == "1":
+        print("")  
+        print("                *******************************************")
+        print("                |                                         |")
+        print("                |          Welcome to admin mode          |")
+        print("                |                                         |")
+        print("                *******************************************")
+        print("")
         Username = input("Please enter your username : ")
         Password = input("Please enter your password : ")
         
         while True:
             if Username == "Admin" and Password == "1234":
-                
-                print("-----------------------------------------")
-                print("|1. To manage patients                  |")
-                print("|2. To manage doctors                   |")
-                print("|To Exit Enter B                     |")
-                print("-----------------------------------------")
+                print("")
+                print("------------------------------------------")
+                print("| 1. To manage patients                  |")
+                print("| 2. To manage doctors                   |")
+                print("| To Exit Enter B                        |")
+                print("------------------------------------------")
+                print("")
                 AdminOptions = input("Enter your choice : ")
                 AdminOptions = AdminOptions.upper()
 
-                if AdminOptions == "1":  # Admin mode --> Pateints Management
-                    print("-----------------------------------------")
-                    print("|1. To add new patient                  |")
-                    print("|2. Display patient                     |")
-                    print("|3. Delete patient data                 |")
-                    print("|4. Edit patient data                   |")
-                    print("|To Exit enter B                        |")
-                    print("-----------------------------------------")
-                    Admin_choice = input("Enter your choice : ")
-                    Admin_choice = Admin_choice.upper()
+                if AdminOptions == "1":  # Admin mode --> Patients Management
+                    
+                    while True:
+                        Patient.patient_menu()
+                        print("")
+                        Admin_choice = input("Enter your choice : ")
+                        Admin_choice = Admin_choice.upper()
+                        if Admin_choice == "1":
+                            Patient.add_patient()
 
-                    if Admin_choice == "1":
-                        Patient.add_patient()
+                        elif Admin_choice == "2":
+                            Patient.display_patient()
 
-                    elif Admin_choice == "2":
-                        Patient.display_patient()
+                        elif Admin_choice == "3":   
+                            Patient.delete_patient()
 
-                    elif Admin_choice == "3":   
-                        Patient.delete_patient()
+                        elif Admin_choice == "4":  
+                            Patient.edit_patient()
+                                         
+                        elif Admin_choice == "B": 
+                            break
 
-                    elif Admin_choice == "4":  
-                       Patient.edit_patient() 
-
-                    elif Admin_choice == "B": 
-                        break
-
-                    else:
-                        print("Please enter a correct choice\n")
+                        else:
+                            print("")
+                            print("Please enter a correct choice\n")
 
                 elif AdminOptions == "2":
-                    print("-----------------------------------------")
-                    print("|1. To add new doctor                   |")
-                    print("|2. To display doctor                   |")
-                    print("|3. To delete doctor data               |")
-                    print("|4. To edit doctor data                 |")
-                    print("|To Exit enter B                  |")
-                    print("-----------------------------------------")
-                    Admin_choice = input("Enter your choice : ")
-                    Admin_choice = Admin_choice.upper()
-                    
-                    if Admin_choice == "1":
-                        Doctor.add_doctor()
 
-                    elif Admin_choice == "2":  
-                        Doctor.display_doctor()
+                    while True:
+                        Doctor.doctor_menu()
+                        print("")
+                        Admin_choice = input("Enter your choice : ")
+                        Admin_choice = Admin_choice.upper()
+                        
+                        if Admin_choice == "1":
+                            Doctor.add_doctor()
 
-                    elif Admin_choice == "3":  
-                        Doctor.delete_doctor()
+                        elif Admin_choice == "2":  
+                            Doctor.display_doctor()
 
-                    elif Admin_choice == "4":  
-                        Doctor.delete_doctor()
+                        elif Admin_choice == "3":  
+                            Doctor.delete_doctor()
 
-                    elif Admin_choice == "B":  
-                        break
+                        elif Admin_choice == "4":  
+                            Doctor.edit_doctor()
 
-                    else:
-                        print("\nPlease enter a correct choice\n")
+                        elif Admin_choice == "B":  
+                            break
+
+                        else:
+                            print("\nPlease enter a correct choice\n")
                 
                 elif AdminOptions == "B":
                     break
@@ -141,21 +140,16 @@ while tries_flag != "Close the program":
                     tries_flag = "Close the program"
                     break
 
-    elif Admin_user_mode == "2": 
-        print("*******************************************")
-        print("|                                         |")
-        print("|         Welcome to User mode            |")
-        print("|                                         |")
-        print("*******************************************")
+    elif Admin_mode == "2": 
+        print("")
+        print("                *******************************************")
+        print("                |                                         |")
+        print("                |         Welcome to User mode            |")
+        print("                |                                         |")
+        print("                *******************************************")
 
         while True:
-            print("-------------------------------------------")
-            print("|1. To view hospital's departments        |")
-            print("|2. To view hospital's doctors            |")
-            print("|3. To view patients' residents           |")
-            print("|4. To view patient's details             |")
-            print("|To Exit Enter B                       |")
-            print("-------------------------------------------")
+            Hospital.hospital_menu()
             UserOptions = input("Enter your choice : ")
             UserOptions = UserOptions.upper()
 
@@ -168,7 +162,7 @@ while tries_flag != "Close the program":
                 Hospital.hospital_doctors()
 
             elif UserOptions == "3":  
-                Hospital.hospital_residents
+                Hospital.hospital_residents()
 
             elif UserOptions == "4": 
                 Patient.display_patient()
@@ -179,5 +173,10 @@ while tries_flag != "Close the program":
             else:
                 print("Please Enter a correct choice")
 
+    elif Admin_mode == "3":
+        print("")
+        print("Program shutting down........")
+        break
+    
     else:
         print("Please choice just 1 or 2")
