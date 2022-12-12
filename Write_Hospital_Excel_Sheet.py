83138d33a241e4ee6e7c2f5405bf573a27c51ea4
CandorKimEmanuel <113954371+KimEmanuelCandor@users.noreply.github.com>
2022-12-12 15:56:22 +0800

    Update Write_Hospital_Excel_Sheet.py

--- Write_Hospital_Excel_Sheet.py
+++ Write_Hospital_Excel_Sheet.py
@@ -1,17 +1,17 @@
 def Write_Patients_DataBase (Patients_Database) :
-	myfile = open("Patients_DataBase.csv","w")
+	myfile = open("Patients_DataBase.txt","w")
 	for i in Patients_Database :
-		myfile.write(str(i)+";"+Patients_Database[i][0]+";"+Patients_Database[i][1]+";"+Patients_Database[i][2]+";"+Patients_Database[i][3]+";"+Patients_Database[i][4]+";"+Patients_Database[i][5]+";"+Patients_Database[i][6]+"\n")
+		myfile.write(str(i)+";"+Patients_Database[i][0]+";"+Patients_Database[i][1]+";"+Patients_Database[i][2]+";"+Patients_Database[i][3]+";"+Patients_Database[i][4]+";"+Patients_Database[i][5]+"\n")
 	myfile.close()
 	
 	
 	
 def Write_Doctors_DataBase (Doctors_DataBase) :
-	myfile = open("Doctors_DataBase.csv","w")	
+	myfile = open("Doctors_DataBase.txt","w")	
 	for i in Doctors_DataBase :
 		myfile.write(str(i)+";")
 		for j in Doctors_DataBase[i] :
-			myfile.write(str(j[0])+";"+j[1]+";"+j[2]+";")
+			myfile.write(j[0]+";"+j[1]+";"+j[2]+";")
 		myfile.write("\n")
 	
 	myfile.close()
